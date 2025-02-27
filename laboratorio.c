0a754242d325af687270a9a14ed9aae67a12751c
PainterB33 <thomasarevalo123@gmail.com>
2025-02-27 12:47:53 -0500

    Update laboratorio.c

--- laboratorio.c
+++ laboratorio.c
@@ -22,9 +22,9 @@ int main(int argc, char *argv[]){
 	}
 	int n = (int) atoi(argv[1]); //--> Indica el numero por el quese divide
 	int d = (int) atoi(argv[2]);
-	int num1 = (double) atof(argv[3]);
-	int num2 = (double) atof(argv[4]);
-	int num3 = (double) atof(argv[5]);
+	double num1 = (double) atof(argv[3]);
+	double num2 = (double) atof(argv[4]);
+	double num3 = (double) atof(argv[5]);
 
 	printf("\nSe van a sumar 4 numeros y se dividen entre el primer numero entregado, se puede considerar una clase de promedio");
 	double res = (d+num1+num2+num3)/n;
